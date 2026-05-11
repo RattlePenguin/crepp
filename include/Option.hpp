ab13cf1b705956778f03438432f00019dddebbc6
RattlePenguin <viggo.setiawan@gmail.com>
2026-05-11 23:53:52 +1000

    Option header to contain longopts and shortopts

---
+++ include/Option.hpp
@@ -0,0 +1,15 @@
+/**
+ *  Thinking of mapping shortopts to longopts using a hashmap
+ *  How to also do no_argument / etc, flag field and val
+ */
+
+#include <getopt.h>
+
+const struct option longopts[] {
+	{ "help", no_argument, 0, '?' },
+	{ "version", no_argument, 0, 'V' },
+	{ "file", required_argument, 0, 'f' },
+	{ "invert-match", no_argument, 0, 'v' }, // these are toggles, look at using flag
+	{ "recursive", no_argument, 0, 'r' },
+	{ 0, 0, 0, 0 }
+};
