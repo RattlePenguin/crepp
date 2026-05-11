4d050e090091b91e4fd134cc488e643b7ba79545
RattlePenguin <viggo.setiawan@gmail.com>
2026-05-11 23:55:07 +1000

    Gathering requirements for CreppClass. TODO implement parsing options, performing crepp on one file given simple options

--- include/CreppClass.hpp
+++ include/CreppClass.hpp
@@ -1,12 +1,20 @@
+#include <vector>
+#include <string>
 
 class CreppClass {
 public:
 
 private:
+	bool vInvert {};
+	std::vector<std::string> patterns {};
+
 	/** 
-	 *  Performs crepp on a given filename
+	 *  Performs crepp on a given path
+	 *  Matches lines with each PATTERN in patterns
+	 *  Colour highlights PATTERN in lines (WIP)
+	 *  If multiple files were given, filename should be prepended to line (WIP)
+	 *  If a directory is given and -r (recursive) was not enabled, produce error
 	 */
-	creppFile();
-	checkFileOrDirectory();
-	
+	void getOptions(int argc, char *argv[], )
+	void crepp(std::string filepath);
 };
---
+++ src/CreppClass.cpp
@@ -0,0 +1,7 @@
+#include "CreppClass.hpp"
+
+void CreppClass::getOptions(int argc, char *argv[], ) {
+}
+
+void CreppClass::crepp(std::string filepath) {
+}
