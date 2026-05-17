c254b17ef89deceeb9999823586d591afc4eb624
RattlePenguin <viggo.setiawan@gmail.com>
2026-05-18 03:18:23 +1000

    Moved crepp logic mostly into CreppClass now.

--- main.cpp
+++ main.cpp
@@ -1,62 +1,12 @@
-#include <cstdint>
+#include "CreppClass.hpp"
+
 #include <iostream>
-#include <unistd.h>
 #include <getopt.h>
-#include <unordered_set>
-#include <filesystem>
-#include <fstream>
-
-const std::string _name { "crepp" };
-
-void printUsage();
-void printHelp();
+#include <memory>
 
 int main(int argc, char *argv[]) {
-	// if no args are given
-	if (argc <= 1) {
-		printUsage();
-		return 0;
-	}
-
-	// first non-opt arg is PATTERN
-	std::string_view pattern { argv[optind] };
-
-	// if no subsequent args, read from stdin
-	if (optind + 1 >= argc) {
-		std::string text {};
-		while (getline(std::cin, text)) {
-			if (text.find(pattern) != std::string::npos) {
-				std::cout << text << '\n';
-			}
-		}
-		return 0;
-	}
-
-	// every subsequent arg is a filename
-	for (int i = optind + 1; i < argc; ++i) {
-		// check if arg is a file
-		std::ifstream file(argv[i]);
-
-		if (!file) {
-			std::cout << _name << ": " << argv[i] << ": No such file or directory\n";
-		}
-
-		std::string text {};
-		while (getline(file, text)) {
-			if (text.find(pattern) != std::string::npos) {
-				std::cout << text << '\n';
-			}
-		}
-
-		// get each line and substring check
-		file.close();
-	}
-	
-}
-
-void printUsage() {
-	std::cout << "Usage: 'crepp [FILE]'" << '\n';
-	std::cout << "Try: 'crepp --help' for more information." << '\n';
+	std::unique_ptr<CreppClass> creppInstance { std::make_unique<CreppClass>(argc, argv) };
+	creppInstance.run();
 }
 
 void printHelp() {
